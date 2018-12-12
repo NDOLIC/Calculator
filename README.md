cfb124d6ad8b6d10fa953b8a6f12f61efedcc55b
NDOLIC <tblaguese1@gmail.com>
2018-12-12 17:01:25 +0200

    js,html and  Readme is added

---
+++ README.md
@@ -0,0 +1,20 @@
+# Calculator
+My first landing page of js
+
+## Ndolic
+
+# Description
+
+this project i done it with a curiosity of how calculator works
+
+# project setup instructions
+
+to open this project you must have one web-browser and text editor
+
+# License
+
+This project is protected under terms of MIT license
+
+# Copyright
+
+(c) 2018
---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+    };
+    var number1 = parseInt(prompt("Enter a number:"));
+    var number2 = parseInt(prompt("Enter another number:"));
+    alert(add(number1, number2));
+    
+    
+    var substract = function(number1, number2) {
+    return number1 - number2;
+    }
+    var number1 = parseInt(prompt("enter a number:"));
+    var nuumber2 = parseInt(prompt("enter another number")); 
+    alert(substract(number1, number2));
+    
+    var division = function(number1, number2) {
+    return number1 / number2
+    }
+    var number1 = parseInt(prompt("enter a number"));
+    var number2 = parseInt(prompt("enter another number"));
+    alert(division(number1, number2));
+    
+    
+    var multiplication = function(number1, number2) {
+    return number1 * number2
+    }
+    var number1 = parseInt(prompt("enter a number"));
+    var number2 = parseInt(prompt("enter another number"));
+    alert(multiplication(number1, number2));
+    
+    var exponential = function(number1, number2) {
+    return number1 ^ number2
+    }    
